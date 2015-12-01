3c58f27935e5c500e81778dedea2578a99e64831
Maksim Didinov <m03567890@gmail.com>
2015-12-01 18:40:46 +0200

    Agent fix, DB fix

--- RPS/ValidationModels/CallValidation.cs
+++ RPS/ValidationModels/CallValidation.cs
@@ -24,11 +24,9 @@ namespace RPS.ValidationModels
             DB_9DF713_RPSEntities db = new DB_9DF713_RPSEntities();
             var upd = from call in db.Call where call.id == id select call;
             upd.ToList()[0].Reason = Reason;
-
+            upd.ToList()[0].Status = 1;
+            upd.ToList()[0].DateSolved = DateTime.Now;
             db.SaveChanges();
-
-
-
         }
 
 
